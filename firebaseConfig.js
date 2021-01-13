bf0cbafbf74a1cc7653adeb464271174cf7c59a2
macarr1 <macarr1@lakeheadu.ca>
2021-01-12 20:51:53 -0500

    firebase update

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+
+      var firebaseConfig = {
+        apiKey: "AIzaSyDQCO5r-ICvbJe4FIKkPeq8Ayh-NT_cTqU",
+        authDomain: "pc-builder-283416.firebaseapp.com",
+        databaseURL: "https://pc-builder-283416.firebaseio.com",
+        projectId: "pc-builder-283416",
+        storageBucket: "pc-builder-283416.appspot.com",
+        messagingSenderId: "521975959454",
+        appId: "1:521975959454:web:f2377f67fe2adbdfc8108f",
+        measurementId: "G-26MWMG7FCD"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+      firebase.analytics();
